9be37f5ee3bba261062c4be09d4f2e0bee78118d
83devansh <sharmdevansh83@gmail.com>
2026-04-27 23:24:42 +0530

    Delete pinecone.js

--- pinecone.js
+++
@@ -1,7 +0,0 @@
-import { Pinecone } from "@pinecone-database/pinecone";
-
-export const pc = new Pinecone({
-  apiKey: "pcsk_7WNgbe_6PRZ27n6jDFKhgeVqRmvLyGHdhrinLPU9WjhNVowwGauNRwrmkvYhEg8HAdNZcj"
-});
-
-export const index = pc.index("campuslink-tasks");
